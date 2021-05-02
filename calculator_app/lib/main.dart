2a274fb847bb98e9ca1213d5f33479c771c162e4
Mohamed Bashir <mohamedbashir@Mohameds-MBP.hitronhub.home>
2021-05-01 22:49:07 -0600

    init app

--- calculator_app/lib/main.dart
+++ calculator_app/lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp()); // run
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -20,6 +20,56 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.menu),
+          title: Text('app title'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Container(
+          // height: 400,
+          // width: 400,
+          color: Colors.amber.shade500,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween, // vertical
+            crossAxisAlignment: CrossAxisAlignment.stretch, // horizantal
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'child 1',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+              // SizedBox(height: 20),
+              Text(
+                'child 2',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+              // SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.red),
+                onPressed: () {},
+                child: Text('click me'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
