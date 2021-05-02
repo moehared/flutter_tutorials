7a344a2c48e247fcc84f496da9428d5f204afae3
Mohamed Bashir <mohamedbashir@Mohameds-MBP.hitronhub.home>
2021-05-01 21:09:04 -0600

    init app

---
+++ calculator_app/android/app/src/main/kotlin/calculator/example/com/calculator_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package calculator.example.com.calculator_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- test_run/android/app/src/main/java/app/example/com/test_run/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package app.example.com.test_run;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
